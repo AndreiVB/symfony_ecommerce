071f5a1844218cae704f77b40f2259aabc4ff1a0
AndreiWWW <andreiv.banut@gmail.com>
2022-06-25 03:30:45 +0300

    configured Register Form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,19 +4,42 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
     /**
      * @Route("/register", name="app_register")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            
+            $password = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+         
+        }
         return $this->render('register/index.html.twig', [
             'form' => $form->createView()
         ]);
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
@@ -20,31 +22,41 @@ class RegisterType extends AbstractType
                 'label' => 'First name',
                 'attr' => [
                     'placeholder' => 'Please enter your first name'
-                ]
+                ],
+                'constraints' => new Length(['min' => 3]),
+                'constraints' => new Length(['max' => 30])
             ])
             ->add('lastname', TextType::class, [
                 'label' => 'Last name',
                 'attr' => [
                     'placeholder' => 'Please enter your last name'
-                ]
+                ],
+                'constraints' => new Length(['min' => 3]),
+                'constraints' => new Length(['max' => 30])
             ])
             ->add('email', EmailType::class, [
                 'attr' => [
                     'placeholder' => 'Please enter your email adress'
+                ],
+                'constraints' => new Length(['min' => 3]),
+                'constraints' => new Length(['max' => 50])
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Paswords must match!',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'placeholder' => 'Please enter your password'
                     ]
-            ])
-            ->add('password', PasswordType::class, [
-                'attr' => [
-                'placeholder' => 'Please enter your password'
-                ]
-            ])
-
-            ->add('password_confirm', PasswordType::class, [
-                'mapped' => false,
-                'label'=> 'Confirm password',
-                'attr' => [
-                    'placeholder' => 'Please confirm your password'
-                ]
+                ],
+                'second_options' => [
+                    'label' => 'Password confirm',
+                    'attr' => [
+                        'placeholder' => 'Please confirm your password'
+                    ]
+                    ]               
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Register'
